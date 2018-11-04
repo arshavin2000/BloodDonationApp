7ec6010203248df1b36788b201f3784434e8e584
arshavin2000 <achrefarshavin@gmail.Com>
2018-11-04 14:31:33 +0100

    Static pages
    Splash Screen activity added

--- app/src/main/java/tn/esprit/blooddonationapp/MainActivity.java
+++ app/src/main/java/tn/esprit/blooddonationapp/MainActivity.java
@@ -1,5 +1,6 @@
 package tn.esprit.blooddonationapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(MainActivity.this, SplashScreenActivity.class);
+        startActivity(i);
     }
 }
