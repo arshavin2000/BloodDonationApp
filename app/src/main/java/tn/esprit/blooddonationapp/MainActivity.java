1f4c3d0038c05ef0e71a1abb6c0085d1a6f7c49a
arshavin2000 <achrefarshavin@gmail.Com>
2018-11-04 16:09:26 +0100

    Login
    Login static page

--- app/src/main/java/tn/esprit/blooddonationapp/MainActivity.java
+++ app/src/main/java/tn/esprit/blooddonationapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent i = new Intent(MainActivity.this, SplashScreenActivity.class);
+        Intent i = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(i);
     }
 }
