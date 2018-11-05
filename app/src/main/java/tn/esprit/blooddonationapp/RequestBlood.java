c23d90a86738eca5df459a2f615b8730390a764a
Unknown <haffez23@gmail.com>
2018-11-05 10:05:43 +0100

    Request and Donor Pages
    Add Static Pages

--- app/src/main/java/tn/esprit/blooddonationapp/RequestBlood.java
+++ app/src/main/java/tn/esprit/blooddonationapp/RequestBlood.java
@@ -3,11 +3,11 @@ package tn.esprit.blooddonationapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class UserActivitiesActivity extends AppCompatActivity {
+public class RequestBlood extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_activities);
+        setContentView(R.layout.activity_request_blood);
     }
 }
--- app/src/main/java/tn/esprit/blooddonationapp/RequestList.java
+++ app/src/main/java/tn/esprit/blooddonationapp/RequestList.java
@@ -1,17 +1,13 @@
 package tn.esprit.blooddonationapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RequestList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent i = new Intent(MainActivity.this, UserPostsActivity.class);
-        startActivity(i);
+        setContentView(R.layout.activity_request_list);
     }
 }
