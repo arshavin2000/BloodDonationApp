b9b3520ab9b6a5ab625e32a697f31a513e33a908
arshavin2000 <achrefarshavin@gmail.Com>
2018-11-05 02:09:18 +0100

    list_display item
    fix some troubles

--- app/src/main/java/tn/esprit/blooddonationapp/MainActivity.java
+++ app/src/main/java/tn/esprit/blooddonationapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent i = new Intent(MainActivity.this, BecomeDonorActivity.class);
+        Intent i = new Intent(MainActivity.this, UserActivitiesActivity.class);
         startActivity(i);
     }
 }
