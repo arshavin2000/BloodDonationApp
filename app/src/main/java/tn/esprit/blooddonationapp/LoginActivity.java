6b86ca88d634a00daac75aa4f4f891baf1ca7f87
Achref Meghirbi <medachref.meghirbi@esprit.tn>
2018-11-08 20:22:04 +0100

    get fb info
    log with fb and retrieve profile informations

--- app/src/main/java/tn/esprit/blooddonationapp/LoginActivity.java
+++ app/src/main/java/tn/esprit/blooddonationapp/LoginActivity.java
@@ -3,21 +3,33 @@ package tn.esprit.blooddonationapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
     private  Button mGoogle , mPhoneNumber;
     private LoginButton mFacebook;
     private CallbackManager callbackManager;
+    private String LOG_TAG ="FB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +41,15 @@ public class LoginActivity extends AppCompatActivity {
         mGoogle =findViewById(R.id.google);
         mPhoneNumber = findViewById(R.id.phone);
 
-        mFacebook.setReadPermissions("email");
-        // If using in a fragment
-        //mFacebook.setFragment(this);
+        mFacebook.setReadPermissions("pro");
+
+
+        final AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        final boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+        LoginManager.getInstance().logInWithReadPermissions(
+                this,
+                Arrays.asList("public_profile"));
+
 
         // Callback registration
         mFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -39,17 +57,29 @@ public class LoginActivity extends AppCompatActivity {
             public void onSuccess(LoginResult loginResult) {
                 // App code
 
-                Toast.makeText(getApplicationContext(),"Facebook",Toast.LENGTH_LONG).show();
+                if(isLoggedIn)
+                {
+
+                    getFbInfo();
+
+
+                }
             }
 
             @Override
             public void onCancel() {
                 // App code
+
+                Toast.makeText(getApplicationContext(),"Facebook Cancel",Toast.LENGTH_LONG).show();
+
             }
 
             @Override
             public void onError(FacebookException exception) {
                 // App code
+                Toast.makeText(getApplicationContext(),"Facebook" + exception.toString(),Toast.LENGTH_LONG).show();
+
+
             }
         });
 
@@ -62,6 +92,43 @@ public class LoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void getFbInfo() {
+        GraphRequest request = GraphRequest.newMeRequest(
+                AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(
+                            JSONObject object,
+                            GraphResponse response) {
+                        try {
+                            Log.d(LOG_TAG, "fb json object: " + object);
+                            Log.d(LOG_TAG, "fb graph response: " + response);
+
+                            String id = object.getString("id");
+                            String first_name = object.getString("first_name");
+                            String last_name = object.getString("last_name");
+                            String gender = object.getString("gender");
+                            String birthday = object.getString("birthday");
+                            String image_url = "http://graph.facebook.com/" + id + "/picture?type=large";
+                            Toast.makeText(getApplicationContext(),"Hello" + first_name + " + "+ last_name,Toast.LENGTH_LONG).show();
+
+
+                            String email;
+                            if (object.has("email")) {
+                                email = object.getString("email");
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,first_name,last_name,email,gender,birthday"); // id,first_name,last_name,email,gender,birthday,cover,picture.type(large)
+        request.setParameters(parameters);
+        request.executeAsync();
     }
 
 }
