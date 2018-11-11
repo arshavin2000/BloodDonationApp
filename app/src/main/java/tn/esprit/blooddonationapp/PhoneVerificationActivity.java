9387c7266fe30712d7a208c2f9c5c56902887b42
arshavin2000 <achrefarshavin@gmail.Com>
2018-11-11 16:35:59 +0100

    fix all errors
    fix android studio bugs

--- app/src/main/java/tn/esprit/blooddonationapp/PhoneVerificationActivity.java
+++ app/src/main/java/tn/esprit/blooddonationapp/PhoneVerificationActivity.java
@@ -41,23 +41,14 @@ public class PhoneVerificationActivity extends AppCompatActivity {
         mSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-<<<<<<< HEAD
 
                /* Intent in = new Intent(PhoneVerificationActivity.this, VerifyMobile.class);
-                in.putExtra("app_id", "YOUR_APP_ID_HERE");
-                in.putExtra("access_token","YOUR_ACCESS_TOKEN_HERE");
-                in.putExtra("mobile", "mobile number for verification");
-
-                startActivityForResult(in,VerifyMobile.REQUEST_CODE);*/
-=======
-                view.startAnimation(animation);
-                Intent in = new Intent(PhoneVerificationActivity.this, PhoneVerificationActivity.class);
                 in.putExtra("app_id", "9da88a8819e143959dfe316");
                 in.putExtra("access_token","5773dc4e7f11f8f258107c9dfcda2592b0c37972");
                 in.putExtra("mobile", "00216"+mNumber.getText().toString());
-             //   startActivityForResult(in,PhoneVerificationActivity.REQUEST_CODE);
+
+                startActivityForResult(in,VerifyMobile.REQUEST_CODE);*/
                 finish();
->>>>>>> fc1ead4d00cd16248fe0ccd5c41571491f11bc35
 
             }
         });
@@ -82,22 +73,16 @@ public class PhoneVerificationActivity extends AppCompatActivity {
     protected void onActivityResult(int arg0, int arg1, Intent arg2) {
 // TODO Auto-generated method stub
         super.onActivityResult(arg0, arg1, arg2);
-<<<<<<< HEAD
        /* if (arg0 == VerifyMobile.REQUEST_CODE) {
-=======
-        if (arg0 == VerifyMobile.REQUEST_CODE) {
->>>>>>> fc1ead4d00cd16248fe0ccd5c41571491f11bc35
+
             String message = arg2.getStringExtra("message");
             int result=arg2.getIntExtra("result", 0);
             Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
             Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-<<<<<<< HEAD
         }*/
-=======
         }
->>>>>>> fc1ead4d00cd16248fe0ccd5c41571491f11bc35
 
     }
 
 
-}
+
