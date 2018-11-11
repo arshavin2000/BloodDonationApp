068949b9f2371b12bbe9ecdcd717e462aafbf685
arshavin2000 <achrefarshavin@gmail.Com>
2018-11-11 15:25:31 +0100

    add jar
    add verification phone number jar to libs folder

--- app/src/main/java/tn/esprit/blooddonationapp/PhoneVerificationActivity.java
+++ app/src/main/java/tn/esprit/blooddonationapp/PhoneVerificationActivity.java
@@ -9,17 +9,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseException;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthProvider;
 
-import java.util.concurrent.TimeUnit;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class PhoneVerificationActivity extends AppCompatActivity {
 
@@ -36,18 +28,22 @@ public class PhoneVerificationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_verification);
 
+        firebaseAuth = FirebaseAuth.getInstance();
         mCountry = findViewById(R.id.country_number);
         mNumber = findViewById(R.id.phone_number);
         mSave  = findViewById(R.id.save);
         mVerify = findViewById(R.id.validate);
         mSMS = findViewById(R.id.sms);
+        mSMS.setVisibility(View.VISIBLE);
+        mVerify.setVisibility(View.VISIBLE);
 
 
         mSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                sendVerificationNumber();
+
+
 
             }
         });
@@ -56,9 +52,7 @@ public class PhoneVerificationActivity extends AppCompatActivity {
         mVerify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String code = mSMS.getText().toString();
-                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codeSent,code);
-                signInWithPhoneAuthCredential(credential);
+
             }
         });
 
@@ -70,75 +64,5 @@ public class PhoneVerificationActivity extends AppCompatActivity {
 
     }
 
-    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
-        firebaseAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
 
-                            Intent intent = new Intent(PhoneVerificationActivity.this,BecomeDonorActivity.class);
-                            startActivity(intent);
-                            finish();
-                            // ...
-                        } else {
-                            // Sign in failed, display a message and update the UI
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-                                // The verification code entered was invalid
-                            }
-                        }
-                    }
-                });
-    }
-
-
-    private void sendVerificationNumber()
-    {
-
-        if(mNumber.getText().toString().equals(""))
-        {
-            mNumber.setError("Phone number is required");
-            mNumber.requestFocus();
-            return;
-        }
-
-        if(mNumber.getText().toString().length()< 8)
-        {
-            mNumber.setError("Please enter a valid phone number");
-            mNumber.requestFocus();
-            return;
-
-        }
-
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                "+216"+mNumber.getText().toString(),        // Phone number to verify
-                60,                 // Timeout duration
-                TimeUnit.SECONDS,   // Unit of timeout
-                this,               // Activity (for callback binding)
-                mCallbacks);        // OnVerificationStateChangedCallbacks
-
-
-    }
-
-    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-        @Override
-        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
-
-        }
-
-        @Override
-        public void onVerificationFailed(FirebaseException e) {
-
-        }
-
-
-        @Override
-        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-            super.onCodeSent(s, forceResendingToken);
-            codeSent =s;
-        }
-    };
 }
