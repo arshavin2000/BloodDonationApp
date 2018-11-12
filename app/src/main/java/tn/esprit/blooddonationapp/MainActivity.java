d8ffa771a3fad3e54f39973e2ae4888f897b6b4a
Achref Meghirbi <medachref.meghirbi@esprit.tn>
2018-11-12 02:24:37 +0100

    add menu

--- app/src/main/java/tn/esprit/blooddonationapp/MainActivity.java
+++ app/src/main/java/tn/esprit/blooddonationapp/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent i = new Intent(MainActivity.this, LoginActivity.class);
+        Intent i = new Intent(MainActivity.this, WelcomeActitvity.class);
         startActivity(i);
     }
 }
